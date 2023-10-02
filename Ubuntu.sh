7cb7ad8a15cc1d171b966fc9d7961d780159627a
MCES10 <138902870+MCES10@users.noreply.github.com>
2023-10-02 13:51:58 +0100

    Update Ubuntu.sh
    Removed Fail2ban

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -36,12 +36,6 @@ sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 sudo systemctl reload ssh
 
-# Install and configure Fail2ban
-echo "Installing and configuring Fail2ban..."
-sudo apt install fail2ban -y
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
-
 # Install unattended-upgrades for automatic security updates
 echo "Setting up automatic security updates..."
 sudo apt install unattended-upgrades -y
